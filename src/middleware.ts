32206204fda8a41600550a3f31913a8305bcc306
Cesar Freire <iceesar@live.com>
2025-06-22 18:19:59 -0300

    fix: Valida autenticação endpoint ação

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,11 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname === "/api/action") {
+    // If the user is not authenticated and trying to access the action API, return an error
+    return new Response(JSON.stringify({ error: "Unauthorized access" }), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+});
